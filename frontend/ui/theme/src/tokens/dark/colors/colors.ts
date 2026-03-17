6e8796537b02cef48510a46482b71ac893128664
Arina Gazhina <gazhina@internet.ru>
2026-03-18 00:23:30 +0300

    feat(ui-theme): dark theme colors

---
+++ frontend/ui/theme/src/tokens/dark/colors/colors.ts
@@ -0,0 +1,40 @@
+export const main = {
+  'main.white': 'rgba(255, 255, 255, 1)',
+  'main.black': 'rgba(0, 0, 0, 1)',
+  'main.transparent': 'rgba(255, 255, 255, 0)',
+}
+
+export const primary = {
+  'primary.main': 'rgba(94, 97, 234, 1)',
+  'primary.background': 'rgba(31, 30, 36, 1)',
+  'primary.link': 'rgba(132, 134, 245, 1)',
+  'primary.selection': 'rgba(37, 40, 195, 0.7)',
+  'primary.heavy': 'rgba(153, 155, 249, 1)',
+}
+
+export const accent = {
+  'accent.info-light': 'rgba(143, 82, 204, 0.3)',
+  'accent.info': 'rgba(199, 169, 230, 1)',
+  'accent.warning-light': 'rgba(255, 190, 92, 0.2)',
+  'accent.warning': 'rgba(255, 197, 108, 1)',
+  'accent.success': 'rgba(95, 184, 165, 1)',
+}
+
+export const contrast = {
+  'contrast.higher': 'rgba(255, 255, 255, 0.85)',
+  'contrast.high': 'rgba(255, 255, 255, 0.7)',
+  'contrast.medium': 'rgba(255, 255, 255, 0.5)',
+  'contrast.low': 'rgba(255, 255, 255, 0.3)',
+  'contrast.lower': 'rgba(255, 255, 255, 0.15)',
+}
+
+export const contrastInverted = {
+  'contrastInverted.higher': 'rgba(0, 0, 0, 0.9)',
+  'contrastInverted.high': 'rgba(0, 0, 0, 0.85)',
+  'contrastInverted.lower': 'rgba(0, 0, 0, 0.7)',
+}
+
+export const contrastLight = {
+  'contrastLight.higher': 'rgba(255, 255, 255, 0.85)',
+  'contrastLight.high': 'rgba(255, 255, 255, 0.7)',
+}
