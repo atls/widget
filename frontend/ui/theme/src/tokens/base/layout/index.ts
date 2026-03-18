0cd400cb57c4fcf7cfade3d1e3d01049f49936a6
Arina Gazhina <gazhina@internet.ru>
2026-03-18 03:23:07 +0300

    feat(ui-theme): layout tokens

---
+++ frontend/ui/theme/src/tokens/base/layout/index.ts
@@ -0,0 +1,9 @@
+import { radii }  from './radii.js'
+import { spaces } from './spaces.js'
+import { zIndex } from './z-index.js'
+
+export const layoutThemeTokens = {
+  spaces,
+  zIndex,
+  radii,
+} as const
---
+++ frontend/ui/theme/src/tokens/base/layout/z-index.ts
@@ -0,0 +1,15 @@
+export const zIndex = {
+  zero: '0',
+  absolute: '9999',
+
+  'bottom.reduced': '1',
+  'bottom.default': '5',
+
+  'low.default': '100',
+
+  'raised.default': '1000',
+
+  'high.default': '2000',
+
+  'peak.default': '3000',
+} as const
